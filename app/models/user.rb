ac9407206511388fc481780de888d6dc4e98b4f1
Allen Shin <geeuho@gmail.com>
2019-11-14 12:13:51 -0800

    Some functionality added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
     has_many :sandwich_reviews
     has_many :sandwiches, through: :sandwich_reviews
-
-    
 end
--- lib/cli.rb
+++ lib/cli.rb
@@ -4,6 +4,7 @@ puts "} Let's make a sandwich! {"
 puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 puts "♫♪.ılılıll|̲̅̅●̲̅̅|̲̅̅=̲̅̅|̲̅̅●̲̅̅|llılılı.♫♪"
 puts 
+
 current_user = nil
 
 def welcome
@@ -11,8 +12,11 @@ def welcome
     prompt = gets.chomp
     if prompt == "1"
         user_create
-    else 
+    elsif prompt == "2"
         log_in
+    else 
+        puts "Sorry that was invalid input, try again!"
+        welcome
     end
 end
 
@@ -23,8 +27,7 @@ def user_create
     puts "Next enter a new password."
     password = gets.chomp
     current_user = User.find_or_create_by(username: user_name, password: password)
-    p current_user
-    # menu(current_user)
+    menu(current_user)
 end
 
 
@@ -43,29 +46,106 @@ def menu(current_user)
     puts "1. Find a new sandwich recipe"
     puts "2. Write a new review"
     puts "3. See reviews you've made"
+    puts "4. Logout"
     menu_prompt = gets.chomp
         if menu_prompt == "1"
             make_sandwich(current_user)
         elsif menu_prompt == "2"
             write_review(current_user)
+        elsif menu_prompt == "3"
+            my_reviews(current_user)
+        elsif menu_prompt == "4"
+            puts "Thanks for stopping by!"
+            return 
         else 
-            current_user.sandwich_reviews
+            puts "That was an invalid response. Please try again."
+            menu(current_user)
         end
 end
 
-def write_review(current_user)
-    puts "Please put a rating!"
-    rating = gets.chomp
-    puts "Please put a review!"
-    review = gets.chomp 
-    SandwichReview.create(rating: rating, review: review, user_id: current_user.id)
+def my_reviews(current_user)
+    if current_user.sandwich_reviews.length == 0
+        puts "You don't have any reviews yet!"
+    else
+        sandwich_reviews_array = current_user.sandwich_reviews
+        i = 0 
+        while i < sandwich_reviews_array.length
+            if sandwich_reviews_array[i].review != nil
+            puts "#{sandwich_reviews_array[i].sandwich.name}"
+            puts "#{sandwich_reviews_array[i].review}"
+            puts "You rated this sandwich #{sandwich_reviews_array[i].rating} stars out of 5"
+            star_wall_thing
+            end
+            i += 1
+        end
+    end
+    menu(current_user)
 end
 
+ 
 
+def write_review(current_user)
+    puts "Which sandwich would you like to review? Please select a number."
+    your_sandwiches = current_user.sandwiches
+    sandwich_list(your_sandwiches)
+    sandwich_select = gets.chomp
+    sandwich_number = sandwich_select.to_i
+    current_sandwich = your_sandwiches[sandwich_number-1]
+    current_review = SandwichReview.find_by(user_id: current_user.id, sandwich_id: current_sandwich.id)
+    puts current_review.review
+    puts current_review
+    puts current_review.sandwich_id
+        if current_review.review == nil
+            puts "no"
+            puts "Please put a rating! (#1-5)"
+            rating = gets.chomp
+            puts "Please put a review!"
+            review = gets.chomp 
+            current_review.update(rating: rating, review: review, user_id: current_user.id, sandwich_id: current_sandwich.id)
+            # menu(current_user)
+        else
+            puts "You already have a review for this sandwich! Did you want to update it (Y/N)?"
+            user_input = gets.chomp
+                if user_input == "Y"
+                    puts "Please put a rating! (#1-5)"
+                    rating = gets.chomp
+                    puts "Please put a review!"
+                    review = gets.chomp 
+                    current_review.update(rating: rating, review: review, user_id: current_user.id, sandwich_id: current_sandwich.id)
+                else 
+                    menu(current_user)
+                end
+        end
+end
 
+def make_sandwich(current_user)
+    puts "Please input ingredients you would like to add to your sandwich"
+    kind_of_sandwich = gets.chomp
+    sandwiches = RestClient.get("http://www.recipepuppy.com/api/?q=BLT, #{kind_of_sandwich}, sandwich")
+    recipe_hash = JSON.parse(sandwiches)
+    current_sandwiches = []
+        recipe_hash["results"].each do |recipe|
+            current_sandwiches << Sandwich.find_or_create_by(name: recipe["title"], recipe_arr: recipe["ingredients"])
+        end
+    puts "Which sandwich would you like to make? Please select a number."
+    sandwich_list(current_sandwiches)
+    sandwich_select = gets.chomp
+    sandwich_number = sandwich_select.to_i
+    current_sandwich = current_sandwiches[sandwich_number-1]
+    puts "You selected the #{current_sandwich.name}"
+    puts "To make this sandwich you need... #{current_sandwich.recipe_arr}!"
+    SandwichReview.find_or_create_by(user_id: current_user.id, sandwich_id: current_sandwich.id)
+    menu(current_user)
+    # puts current_user.sandwiches   
+end
 
-
-
+def sandwich_list(sandwiches)
+    i = 0 
+    while i < sandwiches.length
+        puts "#{i+1}. #{sandwiches[i].name}"
+        i+= 1
+    end
+end
 
 # if User.all.select {|user| user.password == password_input}
 #     password
@@ -76,18 +156,11 @@ end
 # ingredients_input = gets.chomp
 # end
 
-# def results
-# puts "Ooh, sounds yummy! Here are some sandwiches you could make:"
-# octothorpe_wall = "#" * 79
-# star_wall_thing = "*-" * 39 + "*"
-# #enter database to retrieve recipes matching ingredients
-# # .each do |recipe|
-# #     puts octothorpe_wall
-# #     puts
-# #     puts "#{book[:title]} by #{book[:authors]}"
-# #     puts star_wall_thing
-# #     puts book[:description]
-# #     puts
-# end
-# puts octothorpe_wall
+def octothorpe_wall 
+    puts "#" * 79
+end
+
+def star_wall_thing 
+    puts "*-" * 39 + "*"
+end
 
